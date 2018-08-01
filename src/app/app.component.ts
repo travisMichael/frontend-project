b6794db8a644fd2043b9c041e898573e05e67109
Travis Latzke <tlatzke01@gmail.com>
2018-08-01 17:31:40 -0500

    created frontend skeleton

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'event-app';
+  data = null;
+
+  constructor(private http:HttpClient) {
+    console.log("constructor");
+  }
+
+  ngOnInit() {
+    console.log("hello");
+    let result: any = this.http
+      .get("http://localhost:8080/events")
+      .subscribe(response => this.data = response._embedded.events;
+    console.log(this.data);
+  }
+}
