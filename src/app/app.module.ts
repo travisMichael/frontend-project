5f54922545b87b9c9b832ab67eb1be6c581dbe11
Travis Latzke <tlatzke01@gmail.com>
2018-08-03 18:00:56 -0500

    html and css courteous of Anne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MusicComponent } from './music/music.component';
+import { ComedyComponent } from './comedy/comedy.component';
+import { FineArtComponent } from './fine-art/fine-art.component';
+import { FoodSpiritsComponent } from './food-spirits/food-spirits.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'music', component: MusicComponent },
+  { path: 'comedy', component: ComedyComponent },
+  { path: 'fine-art', component: FineArtComponent },
+  { path: 'food-spirits', component: FoodSpiritsComponent },
+  { path: 'subscribe', component: SubscribeComponent },
+  { path: '', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MusicComponent,
+    ComedyComponent,
+    FineArtComponent,
+    FoodSpiritsComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    //  { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
---
+++ src/app/food-spirits/food-spirits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-spirits',
+  templateUrl: './food-spirits.component.html',
+  styleUrls: ['./food-spirits.component.css']
+})
+export class FoodSpiritsComponent {
+  title = 'event-app';
+}
